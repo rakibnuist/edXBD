9ac7affcc1cd75241529be10a86a38e282f8b231
rakibnuist <rakibnuist@gmail.com>
2025-12-14 19:32:43 +0600

    fix: Add direct Meta Pixel and GA4 script loading
    - Added Meta Pixel direct initialization (fbq)
    - Added GA4 direct initialization (gtag)
    - Kept GTM for redundancy
    - All tracking now loads independently
    - Fixes undefined fbq and gtag on production

--- src/components/EnhancedAnalytics.tsx
+++ src/components/EnhancedAnalytics.tsx
@@ -52,23 +52,80 @@ const EnhancedAnalytics = () => {
       {/* Vercel Analytics */}
       <VercelAnalytics />
 
-      {/* Google Tag Manager - Handles GA4 and Meta Pixel */}
+      {/* Google Tag Manager */}
       {GTM_ID && GTM_ID !== 'GTM-XXXXXXX' && (
+        <Script
+          id="gtm-script"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
+      )}
+
+      {/* Google Analytics 4 (GA4) - Direct Loading */}
+      {GA4_MEASUREMENT_ID && GA4_MEASUREMENT_ID !== 'G-XXXXXXXXXX' && (
         <>
           <Script
-            id="gtm-script"
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA4_MEASUREMENT_ID}`}
+            strategy="lazyOnload"
+          />
+          <Script
+            id="ga4-init"
             strategy="lazyOnload"
             dangerouslySetInnerHTML={{
               __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${GTM_ID}');
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA4_MEASUREMENT_ID}', {
+                  page_path: window.location.pathname,
+                });
               `,
             }}
           />
         </>
+      )}
+
+      {/* Meta Pixel (Facebook Pixel) - Direct Loading */}
+      {META_PIXEL_ID && META_PIXEL_ID !== '1234567890' && (
+        <Script
+          id="meta-pixel"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${META_PIXEL_ID}');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      )}
+
+      {/* Meta Pixel Noscript Fallback */}
+      {META_PIXEL_ID && META_PIXEL_ID !== '1234567890' && (
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
       )}
     </>
   );
